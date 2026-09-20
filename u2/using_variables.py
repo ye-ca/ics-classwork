36d10d72d832931536237a10d025ef161c0ff70c
Jeffrey Z <jeffrey.zhang28@ycdsbk12.ca>
2026-09-20 15:16:03 -0400

    made using_variables.py

---
+++ u2/using_variables.py
@@ -0,0 +1,7 @@
+roomNumber = 113
+e = 2.71828
+classSubject = "Computer Science"
+
+print(f"This is room #{roomNumber}")
+print("Euler's number 'e' approximated to 6 digits is {}".format(e))
+print("My most passionate subject is " + classSubject + ", I find it very interesting. ")
