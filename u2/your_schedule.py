df032c6025d527409fe5fe013dcdeaf6c283711a
Jeffrey Z <jeffrey.zhang28@ycdsbk12.ca>
2026-09-22 10:06:48 -0400

    Made your_schedule.py

---
+++ u2/your_schedule.py
@@ -0,0 +1,27 @@
+course1 = "Physics Grade 11 University Level"
+course2 = "Intro to Computer Science Grade 11" 
+course3 = "Communication Technology Grade 11"
+course4 = "Chemistry Grade 11 University Level"
+course5 = "Manufacturing Engineering Technology Grade 11"
+course6 = "Functions Grade 11 University Level"
+course7 = "Religon Grade 11 University/College Level"
+course8 = "Intro to Anthro Grade 11 University Level"
+teacher1 = "Alexander Howland"
+teacher2 = "Daniel Gallo"
+teacher3 = "Maria Grazia Dragonetti"
+teacher4 = "Anthony Gueli"
+teacher5 = "???"
+teacher6 = "???"
+teacher7 = "???"
+teacher8 = "???"
+
+print("+" + 87 * "-" + "+")
+print("| 1 |{0:>50} |{1:>30} |".format(course1, teacher1))
+print("| 2 |{0:>50} |{1:>30} |".format(course2, teacher2))
+print("| 3 |{0:>50} |{1:>30} |".format(course3, teacher3))
+print("| 4 |{0:>50} |{1:>30} |".format(course4, teacher4))
+print("| 5 |{0:>50} |{1:>30} |".format(course5, teacher5))
+print("| 6 |{0:>50} |{1:>30} |".format(course6, teacher6))
+print("| 7 |{0:>50} |{1:>30} |".format(course7, teacher7))
+print("| 8 |{0:>50} |{1:>30} |".format(course8, teacher8))
+print("+" + 87 * "-" + "+")
